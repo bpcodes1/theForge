af001b0c483cfadc20a7347a8585605646e0834b
Bryan Piedra <piedrabr@oregonstate.edu>
2026-04-05 23:34:49 -0700

    Fix React Router basename and Cloudflare redirect loop

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function ScrollHandler() {
 
 export default function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, '')}>
+    <BrowserRouter>
       <ScrollHandler />
       <Routes>
         <Route path="/" element={<HomePage />} />
